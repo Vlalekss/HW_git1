b82d9265d2e429924c74c2a69392bcc6b795b628
Vladislav Alekseev <vlad.alex14@gmail.com>
2022-11-09 20:57:29 +0500

    name rem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello City!");
-        System.out.println("My name is Vlad!");
+        System.out.println("My name is *****!");
     }
 }
